eb53e780eed224cc955ab63c0b573abdef8964c2
Alijan10 <137033191+Alijan10@users.noreply.github.com>
2023-06-19 22:16:56 +0430

    Create Ali1.py

---
+++ Ali1.py
@@ -0,0 +1,19 @@
+pkg update$ pkg upgrade
+
+$ pkg install python
+
+$ pkg install python2
+
+$ pip install requests
+
+$ pip install mechanize
+
+$ pip install bs4
+
+$ pip install rich
+
+$  pkg  نصب  g
+ایترا
+https://github.com/Alijan10/Ali10.py.gitرا
+  پی . ع
+  للییافگcdag .
